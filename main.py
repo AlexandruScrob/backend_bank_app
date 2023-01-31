38b074d466ad8b090f5acee5b8538eb1ff810ab3
Alexandru <alexader_alexandruscrob@yahoo.com>
2023-01-31 22:40:30 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
-import json
 import aiohttp
 import uuid
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 from core.serializers import RequestModel, TransactionModel, PayTypeEnum
-from api import bank_api
+from api import bank_api, helpers
 
 
 app = FastAPI()
@@ -20,9 +20,10 @@ async def root():
 @app.post(path="/transaction")
 async def transaction(req_data: RequestModel):
     # create 2 wallets async
-
-    wallet_id_1, wallet_id_2 = str(uuid.uuid4()), str(uuid.uuid4())
-
+    wallet_id_1, wallet_id_2 = (
+        helpers.generate_wallet_id(),
+        helpers.generate_wallet_id(),
+    )
     async with aiohttp.ClientSession() as session_1:
         tasks = [
             bank_api.create_wallet(session=session_1, wallet_id=str(uuid.uuid4()))
@@ -30,6 +31,7 @@ async def transaction(req_data: RequestModel):
         ]
         await asyncio.gather(*tasks, return_exceptions=True)
 
+    # make 2 way transactions
     async with aiohttp.ClientSession() as session_2:
         tasks = [
             # from
